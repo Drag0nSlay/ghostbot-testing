1f13f02f8beef464189066036db4aff8e384c703
Amol Gupta <62533644+FighterX777@users.noreply.github.com>
2025-05-30 22:53:43 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask # type: ignore
+import main.py
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'This is test server'
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
